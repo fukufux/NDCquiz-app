50463a5a43bdf1e5a726e397430bb6173c263351
fukufux <tamamorokosi@gmail.com>
2025-06-23 10:49:20 +0900

    Update quiz_app.py

--- quiz_app.py
+++ quiz_app.py
@@ -23,9 +23,9 @@ if st.session_state.current < len(st.session_state.selected_quiz):
     q = st.session_state.selected_quiz[st.session_state.current]
     st.subheader(f"Q{st.session_state.current + 1}: {q['question']}")
 
-    with st.form(key="quiz_form"):
-       user_input = st.text_input("半角数字を入力", value="", key="answer_input", autocomplete="off")
-        submitted = st.form_submit_button("次へ")
+   with st.form(key="quiz_form", clear_on_submit=True):
+    user_input = st.text_input("半角数字を入力してね", value="", key="answer_input", autocomplete="off")
+    submitted = st.form_submit_button("次へ")
 
     if submitted:
         correct = q["answer"].strip()
